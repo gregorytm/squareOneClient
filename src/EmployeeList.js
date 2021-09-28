90805ff6f66304d6ad5130d656078aa7c7a2ff10
Greg Marsh <gregorytm@users.noreply.github.com>
2021-09-28 15:09:45 -0700

    fix to folder structure

--- src/EmployeeList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EmployeeList = ({ username, first_inital, last_name, active }) => {
-  return <h1>this is an employee list</h1>;
-};
-
-export default EmployeeList;
